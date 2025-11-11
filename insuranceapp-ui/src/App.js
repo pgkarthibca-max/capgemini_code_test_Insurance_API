727ad62b02c2364d953b531c171d612f4079480d
pgkarthibca-max <pgkarthi.bca@gmail.com>
2025-11-11 17:54:03 +0530

    UI for Insurance Calculator
    All the files uploaded separately

---
+++ insuranceapp-ui/src/App.js
@@ -0,0 +1,11 @@
+import PremiumCalculator from "./components/PremiumCalculator";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <PremiumCalculator />
+    </div>
+  );
+}
+
+export default App;
